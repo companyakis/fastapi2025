c8bb23dfc44cde08e0291c63b803b6803fdeba07
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 09:46:35 +0300

    Create part2-0006-input2-area-calculator.py

---
+++ part2-0006-input2-area-calculator.py
@@ -0,0 +1,9 @@
+width = int(input("Please, integer width: "))
+
+height = int(input("Please, integer height: "))
+
+print(f"Area of the sample rectangular: {width * height}")
+
+# Please, integer width: 45
+# Please, integer height: 12
+# Area of the sample rectangular: 540
