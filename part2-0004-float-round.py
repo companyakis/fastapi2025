06722cf851811c810101679ae3b5375b2b28b691
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 09:38:15 +0300

    Create part2-0004-float-round.py

---
+++ part2-0004-float-round.py
@@ -0,0 +1,11 @@
+period: int = 13
+
+income: float = 55200.65
+
+division: float = income / period
+
+print("Result 1: ", division) # Result 1:  4246.203846153846
+
+print("Result 2: ", round(division, 3)) # Result 2:  4246.204
+
+print("Result 3: ", round(division, 4)) # Result 3:  4246.2038
