c665b0a0948dac1f3b6c8fa80c67f0acf5fbf43e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 10:58:44 +0300

    Create part2-0009-format1.py

---
+++ part2-0009-format1.py
@@ -0,0 +1,9 @@
+name: str = "Mustafa Buyukdereli"
+
+year: int = 2024
+
+pi: float = 3.14
+
+output = "In {}, {} says that pi value is equal to {}."
+
+print(output.format(year, name, pi)) # In 2024, Mustafa Buyukdereli says that pi value is equal to 3.14.
