b39210526561e40304e44b70a49f73b7f203f597
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 10:53:24 +0300

    Create part2-0008-type-conversion3.py

---
+++ part2-0008-type-conversion3.py
@@ -0,0 +1,28 @@
+a: float = 22.14
+
+b: float = 17.87
+
+c: int = 22
+
+d: str = "3.14"
+
+int_a = int(a)
+
+int_b = int(b)
+
+float_c = float(c)
+
+pi = float(d)
+
+print(a, int_a)
+
+print(b, int_b)
+
+print(c, float_c)
+
+print(type(pi))
+
+# 22.14 22
+# 17.87 17
+# 22 22.0
+# <class 'float'>
