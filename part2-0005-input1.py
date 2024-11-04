5d448fb30f5eebfeab3a36bac3fb3323f63ae6b8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 09:42:29 +0300

    Create part2-0005-input1.py

---
+++ part2-0005-input1.py
@@ -0,0 +1,10 @@
+name: str = input("Enter your name: ")
+
+lastname: str = input("Enter your surname: ")
+
+print(f"You are {name} {lastname}")
+
+# Enter your name: Mustafa
+# Enter your surname: Büyükdereli
+# You are Mustafa Büyükdereli
+
