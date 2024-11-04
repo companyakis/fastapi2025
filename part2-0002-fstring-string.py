6c8ba96f55b9a55bf426cf07640508557d6cc150
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 09:29:48 +0300

    Create part2-0002-fstring-string.py

---
+++ part2-0002-fstring-string.py
@@ -0,0 +1,18 @@
+my_name: str = "Mustafa"
+
+my_last_name: str = 'Büyükdereli'
+
+country: str = "Turkiye"
+
+print(my_name + " " + my_last_name)
+
+# or use f-string
+
+print(f"{my_name} {my_last_name}")
+
+print(f"{country} - {my_last_name} - {my_name}")
+
+
+# Mustafa Büyükdereli
+# Mustafa Büyükdereli
+# Turkiye - Büyükdereli - Mustafa
