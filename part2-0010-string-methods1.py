91c3edc79111de9bca80a9653326a004d53fc674
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-11-04 11:06:23 +0300

    Create part2-0010-string-methods1.py

---
+++ part2-0010-string-methods1.py
@@ -0,0 +1,17 @@
+name: str = "    mustafA buyukderEli "
+
+print(name.strip())
+
+print(name.lower())
+
+print(name.upper())
+
+print(name.capitalize())
+
+print(name.title())
+
+
+    # mustafa buyukdereli 
+    # MUSTAFA BUYUKDERELI
+    # mustafa buyukdereli
+    # Mustafa Buyukdereli
