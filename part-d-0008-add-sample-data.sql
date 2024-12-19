7a4e251c9db7cc4066c23343e1cce312c4792f2d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-19 20:54:56 +0300

    Create part-d-0008-add-sample-data.sql

---
+++ part-d-0008-add-sample-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO todos (title, todo_description, todo_importance, is_completed) VALUES ("Math Lesson", "Lorem ipsum", 1, False)
+
+INSERT INTO todos (title, todo_description, todo_importance, is_completed) VALUES ("Clean", "Clean the room regularly", 2, True)
